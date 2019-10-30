8860fe73b9c696231d8f8c43d59a3ba2e78c84e1
s-akhmedoff <sodikjon.akhmedoff@gmail.com>
2019-10-30 16:17:09 +0500

    init update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# mysandbox
+# My Own SandBox
 My own sandbox for solving some problems on C
+
+* C11 
+* x86_64
+* AMD
+
+## Some Tips To Navigate
+// TODO
+
+## One More Section
+// TODO
---
+++ main.c
@@ -0,0 +1,14 @@
+//
+// Created by sakhmedoff on 30.10.2019.
+#include "stdio.h"
+#define DEFAULT_RETURN 0
+
+int main(void)
+{
+
+    printf("%s", (char *)"Hello");
+
+    return DEFAULT_RETURN;
+}
+//
+
